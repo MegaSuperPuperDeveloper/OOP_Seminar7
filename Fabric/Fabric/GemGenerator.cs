9e954e827b02949eec9fc5e8862ac81fe1f5a3b8
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-03-08 16:12:26 +0300

    End of Seminar

--- Fabric/Fabric/GemGenerator.cs
+++ Fabric/Fabric/GemGenerator.cs
@@ -1,6 +1,12 @@
-﻿namespace Fabric.Fabric;
+﻿using Fabric.Interface;
+using Fabric.Product;
 
-public class GemGenerator
+namespace Fabric.Fabric;
+
+public class GemGenerator : ItemGenerator
 {
-    
+    public override iGameItem CreateItem()
+    {
+        return new Gem();
+    }
 }
